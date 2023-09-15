791f99689490cc24faf82e4d2e3c0dc0fbbc108e
dhruv.p <dhruv.p@webcodegenie.com>
2023-09-15 17:07:41 +0530

    packge upload

---
+++ src/KeywordExtractorServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Wcg\KeywordExtractor;
+
+use Illuminate\Support\ServiceProvider;
+
+class KeywordExtractorServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        
+    }
+
+    public function boot()
+    {
+        
+    }
+}
